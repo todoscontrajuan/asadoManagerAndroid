f5bf410c1a1e60dd73b39dafddcd14e35e61cc51
Juan Manuel Rodriguez <juan.manuel.rodriguez@fisglobal.com>
2018-12-06 11:16:56 -0300

    Migration to Androidx

--- app/src/main/java/com/me/squad/asadomanager/MainActivity.kt
+++ app/src/main/java/com/me/squad/asadomanager/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.me.squad.asadomanager
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
