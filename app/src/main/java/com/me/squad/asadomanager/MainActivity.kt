934b85a7fe0c5e74f68c48bfebeb4c2f7fc9f7d4
Juan Manuel Rodriguez <juan.manuel.rodriguez@fisglobal.com>
2018-12-11 14:04:05 -0300

    Add Asado form UI

--- app/src/main/java/com/me/squad/asadomanager/MainActivity.kt
+++ app/src/main/java/com/me/squad/asadomanager/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.me.squad.asadomanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(bottom_app_bar)
+
         val asadosList: ArrayList<Asado> = arrayListOf(Asado("Mi primer asado", 22, "22/07/09"),
                 Asado("Mi segundo asado", 20, "22/07/09"),
                 Asado("Mi segundo asado", 20, "22/07/09"),
@@ -27,12 +29,16 @@ class MainActivity : AppCompatActivity() {
                 Asado("Mi segundo asado", 20, "22/07/09"),
                 Asado("Mi tercer asado", 3, "22/07/09"))
 
+
         linearLayoutManager = LinearLayoutManager(this)
         asados_container.layoutManager = linearLayoutManager
         adapter = AsadosAdapter(asadosList)
         asados_container.adapter = adapter
 
-        setSupportActionBar(bottom_app_bar)
+        fab.setOnClickListener {
+            val intent = Intent(this, AsadoFormActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
